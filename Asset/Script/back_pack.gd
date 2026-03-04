89bdeaa23c4f289b1f5fb5ffc11eb89f1783373d
PakornkitNaivirat <mazzman111@gmail.com>
2026-03-04 22:12:40 +0700

    HotFiexed - Minigame1

--- Asset/Script/back_pack.gd
+++ Asset/Script/back_pack.gd
@@ -1,29 +1,24 @@
-extends Node2D # หรือ Area2D ขึ้นอยู่กับว่ากระเป๋าคุณสร้างมาจากอะไร
+extends Node2D
 
 @onready var anim = $AnimationPlayer
 @export var full_texture: Texture2D 
 
-var collected_count = 0  # ตัวนับจำนวนของ
-var max_items = 2        # จำนวนของที่ต้องเก็บให้ครบ (Ipad + Laptop = 2)
+var collected_count = 0
+var max_items = 2
 
-# ฟังก์ชันนี้จะถูกเรียกตอนที่เราปล่อยไอเท็มลงกระเป๋า
+
 func add_item():
 	collected_count += 1
 	print("เก็บของได้: ", collected_count, "/", max_items)
 	
-	# 🌟 แอนิเมชันกระเป๋าเด้งรับของ (ย่อ-ขยาย) ด้วย Tween
 	var tween = get_tree().create_tween()
-	# 1. ขยายกระเป๋าขึ้น 1.2 เท่า อย่างรวดเร็ว (0.1 วินาที)
 	tween.tween_property(self, "scale", Vector2(1.2, 1.2), 0.1)
-	# 2. หดกระเป๋ากลับมาขนาดปกติ (1.0 เท่า) ภายใน 0.1 วินาที
 	tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.1)
 	
 	# ถ้าเก็บครบ 2 ชิ้นแล้ว
 	if collected_count >= max_items:
 		print("ของครบแล้ว! เปลี่ยนรูปกระเป๋า!")
 		
-		# เราสามารถให้มันรอจังหวะเด้งเสร็จนิดนึง ค่อยเล่นอนิเมชัน Full ก็ได้
-		# โดยใช้ await ของ tween
 		await tween.finished
 		anim.play("Full")
 		
--- Asset/Script/ipad.gd
+++ Asset/Script/ipad.gd
@@ -16,22 +16,19 @@ func _process(delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		
-		# --- จังหวะคลิก (หยิบ) ---
 		if event.pressed:
 			var distance = get_global_mouse_position().distance_to(global_position)
 			if distance < 100:
 				is_dragging = true
 				
-				# 🌟 แอนิเมชันขยายใหญ่ (สเกล 1.2 เท่า ภายใน 0.1 วินาที)
 				var tween = get_tree().create_tween()
 				tween.tween_property(self, "scale", Vector2(1.2, 1.2), 0.1)
 				
-		# --- จังหวะปล่อยเมาส์ ---
+		
 		elif not event.pressed:
 			if is_dragging:
 				is_dragging = false
 				
-				# 🌟 แอนิเมชันหดกลับขนาดเดิม (สเกล 1.0)
 				var tween = get_tree().create_tween()
 				tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.1)
 				
@@ -39,8 +36,7 @@ func _input(event: InputEvent) -> void:
 					var distance_to_bp = global_position.distance_to(backpack.global_position)
 					if distance_to_bp < 200:
 						
-						# 🎒 บอกกระเป๋าว่า "ฉันลงกระเป๋าแล้วนะ นำไป 1 แต้ม!"
 						if backpack.has_method("add_item"):
 							backpack.add_item()
 							
-						queue_free() # ทำลายไอเท็มทิ้ง (เข้ากระเป๋าไปแล้ว)
+						queue_free()
