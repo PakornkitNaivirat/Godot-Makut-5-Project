59ddf99d7ed1fe1448e2aea61e03d859714bbb7a
68160289 <68160289@go.buu.ac.th>
2026-04-05 21:21:30 +0700

    jj

--- Asset/Script/Cutscreen/cut_screen_bed.gd
+++ Asset/Script/Cutscreen/cut_screen_bed.gd
@@ -1,10 +1,27 @@
 extends Node2D
-
+@export var stage_bgm: AudioStream 
 @onready var anim = $AnimationPlayer
 @export var next_scene_path: String = ""
 
 func _ready():
 	anim.play("Bed")
+	play_stage_music()
+	
+func play_stage_music():
+	# หาโหนด Autoload (เช็คทั้งชื่อ BGM หรือ BGMManager ตามที่คุณอาจจะตั้งไว้)
+	var bgm_node = get_node_or_null("/root/BGM")
+	if not bgm_node:
+		bgm_node = get_node_or_null("/root/BGMManager")
+
+	if bgm_node:
+		if stage_bgm:
+			# กรณีที่ 1: มีการใส่เพลงในช่อง Inspector -> ให้เล่นเพลงนั้น
+			bgm_node.play_music(stage_bgm)
+		else:
+			# กรณีที่ 2: ช่อง Inspector ว่างเปล่า -> ให้หยุดเพลง (เงียบ)
+			bgm_node.stop_music() 
+	else:
+		print("Warning: Autoload BGM/BGMManager not found!")
 
 func finish_cutscene():
 	
